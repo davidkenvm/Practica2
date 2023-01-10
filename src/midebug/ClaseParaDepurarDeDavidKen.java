050ce914a09b0dc06c8d2359d1c0ad18d157eab3
DavidKen <davidkenvm@gmail.com>
2023-01-10 15:46:24 +0100

    primer commit

---
+++ src/midebug/ClaseParaDepurarDeDavidKen.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package midebug;
+/**
+ * @author Profesor
+ *
+ */
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ClaseParaDepurarDeDavidKen {
+
+	private double suma;
+	private List<Double> lista;
+
+	public ClaseParaDepurarDeDavidKen() {
+		System.out.println("construyendo clase");
+	}
+
+	public double echaCuentas(int param1, double res1) {
+		char car1 = 'a';
+		int contador = 5;
+		
+		lista = new LinkedList<Double>();
+		for (int i = 0; i < 100; i++) {
+			lista.add((double) i);
+		}
+		suma = 0.0;
+		for (Double valor : lista) {
+			suma += valor;
+		}
+		echaCuentas(param1, res1);
+		return suma;
+	}
+}
---
+++ src/midebug/PruebaDebuggerDeDavidKen.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package midebug;
+
+/**
+ * @author Profesor
+ *
+ */
+public class PruebaDebuggerDeDavidKen {
+	
+	public static void main(String[] args) {
+//		int param1= 10;
+//		double res1 = 10;
+		
+		double[] vector = new double[1000000000];
+		
+//		ClaseParaDepurarDeDavidKen clase = new ClaseParaDepurarDeDavidKen();
+//		double valor = clase.echaCuentas(param1, res1);
+//		System.out.println("Sale "+valor);
+	}
+}
